1d8c333aa0f62d4df016dbec2e346ca3845b94c6
Hail Mary <41487770+alejosjen@users.noreply.github.com>
2019-01-26 22:19:27 -0700

    change connection.js to include Heroku and JawsDB information

--- config/connection.js
+++ config/connection.js
@@ -6,22 +6,28 @@ const host = process.env.HOST;
 const dbPassword = process.env.PASSWORD;
 const database = process.env.DATABASE;
 
-var connection = mysql.createConnection({
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
     host: host,
-    port: 3306,
     user: "root",
     password: dbPassword,
     database: database
-});
-
+  })
+}
 // Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+connection.connect();
+
+//For local hosting and testing
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
 
 // Export connection for our ORM to use.
 module.exports = connection;
