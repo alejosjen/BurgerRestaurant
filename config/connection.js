84bc603ac816042a586814cae81817e8f8790308
Hail Mary <41487770+alejosjen@users.noreply.github.com>
2019-01-26 23:49:14 -0700

    uploaded giphy of project, will sort out heroku during office hours

--- config/connection.js
+++ config/connection.js
@@ -3,32 +3,32 @@ require("dotenv").config();
 var mysql = require("mysql");
 
 const host = process.env.HOST;
+const PORT = process.env.PORT || 3000;
 const dbPassword = process.env.PASSWORD;
 const database = process.env.DATABASE;
 
-var connection;
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
+// var connection;
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+  var connection = mysql.createConnection({
     host: host,
     port: 3306,
     user: "root",
     password: dbPassword,
     database: database
-  })
-}
+  });
 // Make connection.
-connection.connect();
+// connection.connect();
 
-//For local hosting and testing
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-//   console.log("connected as id " + connection.threadId);
-// });
+// For local hosting and testing
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 
 // Export connection for our ORM to use.
 module.exports = connection;
